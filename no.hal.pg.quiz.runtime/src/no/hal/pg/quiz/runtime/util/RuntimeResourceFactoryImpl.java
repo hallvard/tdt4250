6878ad7b9e8701a8fb6cb57fab40f3f6dd38ed64
hal <hal@dhcp-111-239.idi.ntnu.no>
2015-08-12 15:15:03 +0200

    Project that creates Karaf feature.

---
+++ no.hal.pg.quiz.runtime/src/no/hal/pg/quiz/runtime/util/RuntimeResourceFactoryImpl.java
@@ -0,0 +1,39 @@
+/**
+ */
+package no.hal.pg.quiz.runtime.util;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.emfjson.jackson.resource.JsonResourceFactory;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Resource Factory</b> associated with the package.
+ * <!-- end-user-doc -->
+ * @see no.hal.pg.quiz.runtime.util.RuntimeResourceImpl
+ * @generated
+ */
+public class RuntimeResourceFactoryImpl extends JsonResourceFactory {
+	/**
+	 * Creates an instance of the resource factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public RuntimeResourceFactoryImpl() {
+		super();
+	}
+
+	/**
+	 * Creates an instance of the resource.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Resource createResource(URI uri) {
+		Resource result = new RuntimeResourceImpl(uri);
+		return result;
+	}
+
+} //RuntimeResourceFactoryImpl
---
+++ no.hal.pg.quiz.runtime/src/no/hal/pg/quiz/runtime/util/RuntimeResourceImpl.java
@@ -0,0 +1,27 @@
+/**
+ */
+package no.hal.pg.quiz.runtime.util;
+
+import org.eclipse.emf.common.util.URI;
+import org.emfjson.jackson.resource.JsonResource;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Resource </b> associated with the package.
+ * <!-- end-user-doc -->
+ * @see no.hal.pg.quiz.runtime.util.RuntimeResourceFactoryImpl
+ * @generated
+ */
+public class RuntimeResourceImpl extends JsonResource {
+	/**
+	 * Creates an instance of the resource.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param uri the URI of the new resource.
+	 * @generated
+	 */
+	public RuntimeResourceImpl(URI uri) {
+		super(uri);
+	}
+
+} //RuntimeResourceImpl
