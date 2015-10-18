60d8d2e9deb78c3eaf62bc7218b092eb58d0b516
Jon Espen Ingvaldsen <jon.espen.ingvaldsen@ntnu.no>
2015-10-18 19:11:21 +0200

    Refactored Quiz Web such that data are dynamicly retrieved from the services

--- no.hal.pg.quiz.web/app/AppHelper.js
+++ no.hal.pg.quiz.web/app/AppHelper.js
@@ -28,35 +28,38 @@ var AppHelper = {
 		return serviceUrl;
 	},
 	
-	loadData : function(theUrl) {
-		var isAsync = arguments.length > 1;
+	loadData : function(theUrl, asArray, callback) {
+		var isAsync = typeof callback === 'function';
 		var xmlHttp = new XMLHttpRequest();
 		if (isAsync) {
-			var callback = arguments[1];
 			var responseObject = this.responseObject;
 			var self = this;
 			xmlHttp.onreadystatechange = function() {
 				if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-					callback(self.responseObject(xmlHttp.responseText));
+					callback(self.responseObject(xmlHttp.responseText, asArray));
 				}
 			}
 		}
 		xmlHttp.open("GET", theUrl, isAsync);
 		xmlHttp.send(null);
-		return (isAsync ? null : this.responseObject(xmlHttp.responseText));
+		return (isAsync ? null : this.responseObject(xmlHttp.responseText, asArray));
 	},
 
 	isArray: function(object) {
 		return (Array.isArray && Array.isArray(object));
 	},
 	
-	responseObject: function(responseText) {
+	responseObject: function(responseText, asArray) {
 		var response = responseText;
 		if (typeof response === 'string') {
 			response = JSON.parse(response);
 		}
-		if (this.isArray(response)) {
-			response = response[0];
+		if (asArray != this.isArray(response)) {
+			if (asArray) {
+				response = [response];
+			} else {
+				response = response[0];
+			} 
 		}
 		this.log("Response: " + responseText + " -> " + response, this.LOGGING_INFO);
 		return response;
--- no.hal.pg.quiz.web/app/components/QuizContainer.jsx
+++ no.hal.pg.quiz.web/app/components/QuizContainer.jsx
@@ -1,7 +1,8 @@
 /*
 this.props:
 {
-	serviceUrl : a link to the current quiz
+	serviceUrl : a link to the current quiz,
+	player : id of current player,
 	questions :
 	[ {
 	  "numChoices" : 1,
@@ -48,18 +49,35 @@ this.state:
 var React = require('react');
 var Question = require('./Question.jsx');
 var ButtonInput = require('react-bootstrap').ButtonInput;
+var AppHelper = require('../AppHelper.js');
 
 var QuizContainer = React.createClass({
+    
+     serviceUrl : AppHelper.serviceUrl(window.location.search),
 
-  getInitialState: function () {
-    return {id: 0, content: 'Loading...'};
-  },
 
-  render: function () {
+    getInitialState: function () {
+		var comp = this;
+
+		AppHelper.loadData(this.serviceUrl, true, function(response) {
+			AppHelper.log("Questions retrieved: " + response, AppHelper.LOGGING_INFO);
+			comp.setState({
+				questions : response
+			});
+		});
+		return {
+			//Return initial test data set in html
+			questions : this.props.questions
+		};
+    },
+
+
+
+    render: function () {
     return (
       <div>
       <form className="col s12">
-      {this.props.questions.map(function(item, idx){
+      {this.state.questions.map(function(item, idx){
           return <Question key={idx} data={item} />;
         })}
     <div>
